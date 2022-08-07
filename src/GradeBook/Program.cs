71e205093e908ff1312d69cb75251b35be546898
Abdullah Al Zaied <abdullahxaied@gmail.com>
2022-08-08 04:21:43 +0600

    user input enabled

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,17 +1,21 @@
 ﻿using GradeBook;
 
 var book = new Book("Akash's Grade Book");
-
-book.AddGrade(56.1);
-book.AddGrade(14.5);
-book.AddGrade(87.9);
-book.AddGrade(53.2);
-book.AddGrade(68.9);
-book.AddGrade(33.7);
+while(true)
+{
+    Console.WriteLine("Enter grade or q to quit");
+    var input = Console.ReadLine();
+    if(input == "q")
+    {
+        break;
+    }
+    var grade = double.Parse(input);
+    book.AddGrade(grade);
+}
 
 var result = book.GetStatistics();
 
 Console.WriteLine($"The highest grade is: {result.High}");
 Console.WriteLine($"The lowest grade is: {result.Low}");
 Console.WriteLine($"Average grade is: {result.Average:N2}");
-Console.WriteLine($"Latter grade is: {result.Letter}");
+Console.WriteLine($"Letter grade is: {result.Letter}");
