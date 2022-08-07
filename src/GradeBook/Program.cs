0dd3449b3fea2989f86bcbbf141668b39819bbcf
Abdullah Al Zaied <abdullahxaied@gmail.com>
2022-08-07 15:24:46 +0600

    List and double formatting

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,9 +6,19 @@ else{
     Console.WriteLine("Hello there!");
 }
 
-var numbers = new[] {12.7, 10.3, 9.1, 13.5};
 var result = 0.0;
+var numbers = new[] {12.7, 10.3, 9.1, 13.5};
+var grades = new List<double>(){12.7, 10.3, 9.1, 13.5, 18.1};
+grades.Add(56.1);
+
+foreach(var number in grades){
+    result += number;
+}
+result /= grades.Count;
+Console.WriteLine($"Average score is {result:N2}");
+
+
 foreach(double number in numbers){
     result += number;
 }
-Console.WriteLine(result);
+Console.WriteLine($"Total Number: {result:N2}");
