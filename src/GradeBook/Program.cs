11f14fc5ee6c40fa318c8aa23a369b921c94d31a
Abdullah Al Zaied <abdullahxaied@gmail.com>
2022-08-08 00:31:38 +0600

    code refactored for testing

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,4 +9,8 @@ book.AddGrade(53.2);
 book.AddGrade(68.9);
 book.AddGrade(33.7);
 
-book.ShowStatistics();
+var result = book.GetStatistics();
+
+Console.WriteLine($"The highest grade is: {result.High}");
+Console.WriteLine($"The lowest grade is: {result.Low}");
+Console.WriteLine($"Average grade is: {result.Average:N2}");
--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -7,8 +7,17 @@ public class BookTests
     {
         //Arrange
         var book = new Book("");
+        book.AddGrade(56.1);
+        book.AddGrade(14.5);
+        book.AddGrade(87.9);
+        book.AddGrade(53.2);
+        book.AddGrade(68.9);
+        book.AddGrade(33.7);
         //Act
-
+        var score = book.GetStatistics();
         //Assert
+        Assert.Equal(52.38, score.Average, 2);
+        Assert.Equal(14.5, score.Low, 1);
+        Assert.Equal(87.9, score.High, 1);
     }
 }
